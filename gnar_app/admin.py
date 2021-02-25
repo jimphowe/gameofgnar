1df2f23f9d30533e2c9cd97fdc0bbd8f8685e0a7
Fitzgibbons <admin@gameofgnar.com>
2021-02-25 04:53:42 +0000

    idk at this point, some admin stuff

--- gnar_app/admin.py
+++ gnar_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Climb, GeneralPoints, MeetingAttended, WorkoutAttended, ClimbComplete, MiscellaneousPoints
+from .models import Climb, GeneralPoints, MeetingAttended, WorkoutAttended, ClimbComplete, MiscellaneousPoints, Comment, ResetReport
 admin.site.register(Climb)
 admin.site.register(GeneralPoints)
 admin.site.register(MeetingAttended)
 admin.site.register(WorkoutAttended)
 admin.site.register(ClimbComplete)
 admin.site.register(MiscellaneousPoints)
+admin.site.register(Comment)
+admin.site.register(ResetReport)
